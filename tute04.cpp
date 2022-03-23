dbac581a6f1c3a27cb244642bde37d4771873bfb
Haritha Denuwan <94567436+it21272622@users.noreply.github.com>
2022-03-23 06:54:48 +0000

    Tutorial 2

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,18 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no)
+{
+  int fac = 1;
+  for (int x=no; x >= 1; x--)
+    {
+        fac = fac * x;
+    }
+  return fac;
+}
+
+long nCr(int n, int r)
+{
+  return Factorial(n)/Factorial(r)*Factorial(n-r);
+}
